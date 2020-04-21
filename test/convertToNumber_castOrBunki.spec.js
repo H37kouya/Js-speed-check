1038e9657a13f0e360a630ee5876fb2c25831bef
H37kouya <h37kouya@gmail.com>
2020-04-21 23:05:07 +0900

    正しい値が帰ってくるかの確認をした

--- test/convertToNumber_castOrBunki.spec.js
+++ test/convertToNumber_castOrBunki.spec.js
@@ -1,6 +1,13 @@
 import { measurePerfLog } from "../src/performance";
 
-describe.skip("convertToNumber_castOrBunki", () => {
+describe("convertToNumber_castOrBunki", () => {
+  it("数値が返ってくるか", () => {
+    expect(Number(true)).toBe(1)
+    expect(Number(false)).toBe(0)
+    expect(true ? 1 : 0).toBe(1)
+    expect(false ? 1 : 0).toBe(0)
+  })
+
   it("cast", () => {
     const func = () => {
       for (let i = 0; i < 100000; i++) {
